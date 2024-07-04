f6e2f9efdca3106619c178e23c7fdbc4d745c7e1
Ahmedgit7 <ahmedhshaikh786@gmail.com>
2024-07-04 10:49:44 +0530

    added the variables in the routes.tf

--- terraform/routes.tf
+++ terraform/routes.tf
@@ -2,7 +2,7 @@ resource "aws_route_table" "private" {
   vpc_id = aws_vpc.employee-main.id
 
   route {
-    cidr_block     = "0.0.0.0/0"
+    cidr_block     = var.private_route_cidr_block
     nat_gateway_id = aws_nat_gateway.nat.id
   }
 
@@ -15,7 +15,7 @@ resource "aws_route_table" "public" {
   vpc_id = aws_vpc.employee-main.id
 
   route {
-    cidr_block = "0.0.0.0/0"
+    cidr_block = var.public_route_cidr_block
     gateway_id = aws_internet_gateway.igw.id
   }
 
